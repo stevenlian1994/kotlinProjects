0b585c3585cbf6830951fe487c0f7ba7ac0823ce
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-08-19 20:01:38 -0700

    updated dice roller game

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.Random
+import android.R.attr.bottom
+import android.R.attr.right
+import android.R.attr.top
+import android.R.attr.left
+import android.view.ViewGroup.MarginLayoutParams
+import android.view.animation.AnimationUtils
+import java.util.Timer
+import kotlin.concurrent.schedule
+import android.view.animation.Animation
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,23 +25,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countButton: Button = findViewById(R.id.count_button)
         rollButton.setOnClickListener { rollDice() }
-        countButton.setOnClickListener {countUp()}
     }
 
     private fun rollDice(){
-        val randomInt = Random().nextInt(6) + 1
-        val result_text: TextView = findViewById(R.id.result_text)
-        result_text.text = randomInt.toString()
-//        Toast.makeText(this, randomInt,
-//            Toast.LENGTH_SHORT).show()
+
+        val diceImage1: ImageView = findViewById(R.id.dice_image_1)
+        val diceImage2: ImageView = findViewById(R.id.dice_image_2)
+        val drawableResource1 = getRandomDiceImage()
+        val drawableResource2 = getRandomDiceImage()
+        val animation  = AnimationUtils.loadAnimation(this@MainActivity, R.anim.shake);
+        diceImage1.startAnimation(animation)
+        diceImage2.startAnimation(animation)
+        Timer("SettingUp", false).schedule(500) {
+            diceImage1.setImageResource(drawableResource1)
+            diceImage2.setImageResource(drawableResource2)
+        }
+
+
+//        val myImage = findViewById(R.id.image_view) as ImageView
+//        val marginParams = image.getLayoutParams() as MarginLayoutParams
+//    marginParams.setMargins(left, 82, right, bottom)
+
     }
-    private fun countUp(){
-        if(result_text.text != "Hello World!" && result_text.text != "6" ){
-            val result_text: TextView = findViewById(R.id.result_text)
-            val new_value = result_text.text.toString().toInt() + 1
-            result_text.text = new_value.toString()
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = Random().nextInt(6) + 1
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
+
 }
